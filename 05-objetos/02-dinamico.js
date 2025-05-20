ed1f99a55b4a3917b1849c5d32d01b190fb332e7
Stephen Lucic M. <stephen.lucic.maldonado@gmail.com>
2025-05-20 12:47:45 -0400

    se agrega objetos-dinamico

--- 05-objetos/02-dinamico.js
+++ 05-objetos/02-dinamico.js
@@ -1,8 +1,34 @@
-const user = { id: 1};
+/**
+ * esta constante, no ten deja modificar el tipo de dato
+ * pero si este es un objeto, si se puede  modificar su contentido
+ * por lo tannto, no podría cambiar el tipo a number o string por ejemplo
+ * pero si puedes modificar sus valores, que tiene dentro
+ * del objeto
+ */
+
+const user = { id: 1}; 
 
 user.name = 'Yerko';
 user.guardar = function () {
     console.log('guardando....', user.name);
 }
+console.log(user);
 
 user.guardar();
+
+delete user.name;
+delete user.guardar;
+console.log('user');
+console.warn('user');//este me entrega un warning en consola
+
+//esta funcion no deja que se cambie el valor del objeto
+// const user1 = Object.freeze({id:1}) 
+
+/**
+ * esta funcion deja cambiar los datos
+ * de la propiedad, pero no agregarle nuevas
+ */
+const user1 = Object.seal({id:1})
+user1.name = 'Yerko';
+user1.id=2;
+console.log(user1);
